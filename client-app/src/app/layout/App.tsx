22a35646eda6c97a512dd711dbbc1808a45b28c4
Wei Jing <jaychou123103@hotmail.com>
2020-06-08 06:22:16 +1200

    Clean react folders and get all activities

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,20 +1,18 @@
 import React, { Component } from 'react';
 import { Header, Icon, List } from 'semantic-ui-react'
-import './App.css';
 import axios from 'axios';
 
 class App extends Component {
 
   state = {
-    values: []
+    activities: []
   }
 
   componentDidMount() {
-
-    axios.get('http://localhost:5000/api/values')
+    axios.get('http://localhost:5000/api/activities')
     .then((response) => {
       this.setState({
-        values: response.data
+        activities: response.data
       })
     })
 
@@ -29,8 +27,8 @@ class App extends Component {
           <Header.Content>Activities</Header.Content>
         </Header>
         <List>
-          { this.state.values.map((value: any) => (
-            <List.Item key={value.id}>{value.name}</List.Item>
+          { this.state.activities.map((activity: any) => (
+            <List.Item key={activity.id}>{activity.title}</List.Item>
           ))}
         </List>
       </div>
