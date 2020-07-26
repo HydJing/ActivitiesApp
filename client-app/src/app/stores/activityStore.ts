d5eb642101fda3f2ef3d9c1c02723e9122e140e4
Wei Jing <jaychou123103@hotmail.com>
2020-07-27 00:41:49 +1200

    add isgoing and ishost status for activity

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -5,6 +5,7 @@ import agent from '../api/agent';
 import { history } from '../..';
 import { toast } from 'react-toastify';
 import { RootStore } from './rootStore';
+import { setActivityProps } from '../common/util/util';
 
 export default class ActivityStore {
   rootStore: RootStore;
@@ -46,7 +47,7 @@ export default class ActivityStore {
       const activities = await agent.Activities.list();
       runInAction('loading activities', () => {
         activities.map((activity) => {
-          activity.date = new Date(activity.date);
+          setActivityProps(activity, this.rootStore.userStore.user!);
           this.activityRegistry.set(activity.id, activity);
         });
         this.loadingInitial = false;
@@ -69,7 +70,7 @@ export default class ActivityStore {
       try {
         activity = await agent.Activities.details(id);
         runInAction('getting activity', () => {
-          activity.date = new Date(activity.date);
+          setActivityProps(activity, this.rootStore.userStore.user!);
           this.activity = activity;
           this.activityRegistry.set(activity.id, activity);
           this.loadingInitial = false;
