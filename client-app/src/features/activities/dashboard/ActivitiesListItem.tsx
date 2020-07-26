7f6b5bace295b9e498cff4a32d5ca8b5e63e988b
Wei Jing <jaychou123103@hotmail.com>
2020-07-26 22:54:36 +1200

    add attendee detail to activity and sidebar

--- client-app/src/features/activities/dashboard/ActivitiesListItem.tsx
+++ client-app/src/features/activities/dashboard/ActivitiesListItem.tsx
@@ -3,6 +3,7 @@ import { Item, Button, Segment, Icon } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 import { IActivity } from '../../../app/models/activity';
 import { format } from 'date-fns';
+import ActivityListItemAttendees from './ActivityListItemAttendees';
 
 const ActivitiesListItem: React.FC<{ activity: IActivity }> = ({
   activity,
@@ -25,7 +26,9 @@ const ActivitiesListItem: React.FC<{ activity: IActivity }> = ({
         <Icon name="clock" /> {format(activity.date, 'H:mm a')}
         <Icon name="marker" /> {activity.venue}, {activity.city}
       </Segment>
-      <Segment secondary>Attendees goes here</Segment>
+      <Segment secondary>
+        <ActivityListItemAttendees attendees={activity.attendees} />
+      </Segment>
       <Segment clearing>
         <span>{activity.description}</span>
         <Button
