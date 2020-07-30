688617d73ebcb0caf46312444691961fd7cf93c0
Wei Jing <jaychou123103@hotmail.com>
2020-07-31 01:26:20 +1200

    add image CDN service

---
+++ Infrastructure/Photos/CloudinarySettings.cs
@@ -0,0 +1,9 @@
+namespace Infrastructure.Photos
+{
+    public class CloudinarySettings
+    {
+        public string CloudName { get; set; }
+        public string CloudAPIKey { get; set; }
+        public string CloudAPISecret { get; set; }
+    }
+}
