8a94c1be3a2d30a569d7647479b4cd258523ce7a
Wei Jing <jaychou123103@hotmail.com>
2020-08-09 23:00:22 +1200

    Add profile content

---
+++ client-app/src/features/profiles/ProfileContent.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Tab, Grid} from 'semantic-ui-react';
+import ProfileHeader from './ProfileHeader';
+
+const panes = [
+    {menuItem: 'About', render: () => <Tab.Pane>About Content</Tab.Pane>},
+    {menuItem: 'Photos', render: () => <Tab.Pane>Photos Content</Tab.Pane>},
+    {menuItem: 'Activities', render: () => <Tab.Pane>Activities Content</Tab.Pane>},
+    {menuItem: 'Follower', render: () => <Tab.Pane>Follower Content</Tab.Pane>},
+    {menuItem: 'Following', render: () => <Tab.Pane>Following Content</Tab.Pane>},
+]
+
+const ProfileContent = () => {
+    return (
+        <Tab 
+            menu={{fluid: true, vertical: true}}
+            menuPosition='right'
+            panes={panes}
+        />
+    )
+}
+
+export default ProfileContent
