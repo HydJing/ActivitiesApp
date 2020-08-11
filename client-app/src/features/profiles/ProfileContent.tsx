4418a929a90173e717540331790a89a9f8961798
Wei Jing <jaychou123103@hotmail.com>
2020-08-11 23:48:23 +1200

    add profile store with photos

--- client-app/src/features/profiles/ProfileContent.tsx
+++ client-app/src/features/profiles/ProfileContent.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Tab, Grid} from 'semantic-ui-react';
 import ProfileHeader from './ProfileHeader';
+import ProfilePhotos from './ProfilePhotos';
 
 const panes = [
     {menuItem: 'About', render: () => <Tab.Pane>About Content</Tab.Pane>},
-    {menuItem: 'Photos', render: () => <Tab.Pane>Photos Content</Tab.Pane>},
+    {menuItem: 'Photos', render: () => <ProfilePhotos />},
     {menuItem: 'Activities', render: () => <Tab.Pane>Activities Content</Tab.Pane>},
     {menuItem: 'Follower', render: () => <Tab.Pane>Follower Content</Tab.Pane>},
     {menuItem: 'Following', render: () => <Tab.Pane>Following Content</Tab.Pane>},
