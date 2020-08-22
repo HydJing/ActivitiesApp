d47402f3cb31ab46dc5a0fb6509948a672467b84
Wei Jing <jaychou123103@hotmail.com>
2020-08-22 23:55:02 +1200

    add signalR in client side

--- client-app/src/app/models/activity.ts
+++ client-app/src/app/models/activity.ts
@@ -9,6 +9,16 @@ export interface IActivity {
   isGoing: boolean;
   isHost: boolean;
   attendees: IAttendee[];
+  comments: IComment[];
+}
+
+export interface IComment {
+  id: string;
+  createdAt: Date;
+  body: string;
+  username: string;
+  displayName: string;
+  image: string;
 }
 
 export interface IActivityFormValues extends Partial<IActivity> {
@@ -31,7 +41,6 @@ export class ActivityFormValues implements IActivityFormValues {
     Object.assign(this, init);
   }
 }
-
 
 export interface IAttendee {
   username: string;
