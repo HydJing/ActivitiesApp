3650b8e71925205e2c36d833fe645dc94f012cf7
Wei Jing <jaychou123103@hotmail.com>
2020-08-29 22:34:34 +1200

    add profile reader help class

--- API/Controllers/ProfilesController.cs
+++ API/Controllers/ProfilesController.cs
@@ -8,7 +8,7 @@ namespace API.Controllers
     public class ProfilesController : BaseController
     {
         [HttpGet("{username}")]
-        public async Task<ActionResult<Profiles>> Get(string username) 
+        public async Task<ActionResult<Profile>> Get(string username) 
         {
             return await Mediator.Send(new Details.Query{Username = username});
         }
