54cab932d9bb2289317b140cf7035f1d124ad386
Wei Jing <jaychou123103@hotmail.com>
2020-08-30 00:26:48 +1200

    add get following data

--- API/Controllers/FollowerController.cs
+++ API/Controllers/FollowerController.cs
@@ -1,5 +1,7 @@
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Application.Follower;
+using Application.Profiles;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,13 +13,19 @@ namespace API.Controllers
         [HttpPost("{username}/follow")]
         public async Task<ActionResult<Unit>> Follow(string username)
         {
-            return await Mediator.Send(new Add.Command{Username = username});
-        } 
+            return await Mediator.Send(new Add.Command { Username = username });
+        }
 
         [HttpDelete("{username}/follow")]
         public async Task<ActionResult<Unit>> Unfollow(string username)
         {
-            return await Mediator.Send(new Delete.Command{Username = username});
+            return await Mediator.Send(new Delete.Command { Username = username });
+        }
+
+        [HttpGet("{username}/follow")]
+        public async Task<ActionResult<List<Profile>>> GetFollowings(string username, string predicate)
+        {
+            return await Mediator.Send(new List.Query{Username = username, Predicate = predicate});
         }
     }
 }
