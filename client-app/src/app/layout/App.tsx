493339a1bea6236e58e7434e4898878f48b49973
Wei Jing <jaychou123103@hotmail.com>
2020-09-13 19:41:07 +1200

    add private route and fix loading flash text

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -18,6 +18,7 @@ import { RootStoreContext } from '../stores/rootStore';
 import LoadingComponent from './LoadingComponent';
 import ModalContainer from '../common/modal/ModalContainer';
 import ProfilePage from '../../features/profiles/ProfilePage';
+import PrivateRoute from './PrivateRoute';
 
 const App: React.FC<RouteComponentProps> = ({ location }) => {
   const rootStore = useContext(RootStoreContext);
@@ -46,19 +47,19 @@ const App: React.FC<RouteComponentProps> = ({ location }) => {
             <NavBar />
             <Container style={{ marginTop: '7em' }}>
               <Switch>
-                <Route
+                <PrivateRoute
                   exact
                   path="/activities"
                   component={ActivitiesDashboard}
                 />
-                <Route path="/activities/:id" component={ActivityDetails} />
-                <Route
+                <PrivateRoute path="/activities/:id" component={ActivityDetails} />
+                <PrivateRoute
                   key={location.key}
                   path={['/createActivity', '/manage/:id']}
                   component={ActivityForm}
                 />
-                <Route path="/profile/:username" component={ProfilePage} />
-                <Route component={NotFound} />
+                <PrivateRoute path="/profile/:username" component={ProfilePage} />
+                <PrivateRoute component={NotFound} />
               </Switch>
             </Container>
           </Fragment>
